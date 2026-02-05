09896e5cbd942d2a0fc9fbf9f11a079a4e1a0840
jonathanpriyaraj <jonathanpriyaraj37@gmail.com>
2026-02-05 20:46:51 +0000

    added all the files

--- 13.null-local-exec.tf
+++ 13.null-local-exec.tf
@@ -2,6 +2,7 @@ resource "null_resource" "webservers" {
   provisioner "local-exec" {
     command = <<EOH
       sleep 10
+      ssh-keyscan -t rsa -H invfile >> ~/.ssh/known_hosts 2>/dev/null || true
       ansible -i invfile pvt -m ping
     EOH
   }
